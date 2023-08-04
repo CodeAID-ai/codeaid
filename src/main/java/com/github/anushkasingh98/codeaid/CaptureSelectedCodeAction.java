01758077c2049f31e3f6b0c109d2b70362c416bb
Anushka Singh <anushka@anushkasingh.com>
2023-08-04 17:44:18 -0400

    updating code

--- src/main/java/com/github/anushkasingh98/codeaid/CaptureSelectedCodeAction.java
+++ src/main/java/com/github/anushkasingh98/codeaid/CaptureSelectedCodeAction.java
@@ -2,7 +2,9 @@ package com.github.anushkasingh98.codeaid;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.actionSystem.EditorActionHandler;
 import com.intellij.openapi.util.text.StringUtil;
 import org.apache.tools.ant.Project;
 
@@ -13,13 +15,14 @@ public class CaptureSelectedCodeAction extends AnAction
     @Override
     public void actionPerformed(AnActionEvent e)
     {
-        Project project = e.getProject();
-        Editor editor = project.getInputHandler();
-        Editor editor1;
+        Editor editor = CommonDataKeys.EDITOR.getData(e.getDataContext());
         String selectedText = editor.getSelectionModel().getSelectedText();
         if (selectedText != null) {
             selectedText = StringUtil.escapeStringCharacters(selectedText);
             System.out.println(selectedText);
+        }
+        else{
+            System.out.println("Error: OOPS!");
         }
     }
 }
